cd9bec1ebcbef51cac1fee57739be023879f370f
Gage Emerson <64941719+Gage-Emerson@users.noreply.github.com>
2024-08-07 15:10:40 -0400

    Test2

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('navbar.html')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data => {
+        document.getElementById('navbar').innerHTML = data;
+      })
+      .catch(error => console.error('Error fetching the navbar:', error));
+  });
+  
